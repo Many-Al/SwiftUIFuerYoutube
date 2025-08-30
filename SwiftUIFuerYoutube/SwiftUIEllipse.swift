c4d940702d977a43859c7c58f48c858847c279df
mahmoudalame <alamemany27@gmail.com>
2025-08-30 15:27:01 +0200

    SwiftUIEllipse

---
+++ SwiftUIFuerYoutube/SwiftUIEllipse.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIEllipse.swift
+//  SwiftUIFuerYoutube
+//
+//  Created by Mahmoud Alame on 29.08.25.
+//
+
+import SwiftUI
+
+struct SwiftUIEllipse: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIEllipse()
+}
