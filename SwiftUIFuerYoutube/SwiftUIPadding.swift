b11ba6306157a98e8f002672d634583c4dbbaad7
mahmoudalame <alamemany27@gmail.com>
2025-09-06 19:46:38 +0200

    SwiftUIPadding

--- SwiftUIFuerYoutube/SwiftUIPadding.swift
+++ SwiftUIFuerYoutube/SwiftUIPadding.swift
@@ -8,24 +8,48 @@
 import SwiftUI
 
 struct SwiftUIPadding: View {
-   
-        var body: some View {
-            VStack {
-                Text("Standard Padding")
-                    .font(.largeTitle)
-                    .background(.red)
-                    .padding()
-                    .background(.blue)
-
-                Text("Custom Padding")
-                    .font(.largeTitle)
-                    .background(.red)
-                    .padding(50)  // Größeres Padding für mehr Abstand
-                    .background(.blue)
+    
+    var body: some View {
+        /* VStack {
+         Text("Standard Padding")
+         .font(.largeTitle)
+         .background(.red)
+         .padding()
+         .background(.blue)
+         
+         Text("Custom Padding")
+         .font(.largeTitle)
+         .background(.red)
+         // .padding(.vertical, 20)
+         .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 40))
+         
+         
+         
+         // Größeres Padding für mehr Abstand
+         .background(.blue)
+         }
+         }
+         }
+         */
+        
+        VStack{
+            Rectangle()
+                .fill(.green)
+                //.padding(.horizontal, 20)
+            
+            HStack {
+                Circle()
+                    .fill(.red)
+                Circle()
+                    .fill(.blue)
             }
+            //.padding(.horizontal)
         }
+        .padding(.horizontal, 20) // hik fini a3melha 3ala kil alsaf7a
+        .padding(.vertical, 60)
+        .ignoresSafeArea()// ysawi rectongle 3ala el shasha kilaha men foq
     }
-
+}
 
 #Preview {
     SwiftUIPadding()
