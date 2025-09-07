4315b108c212d53a37fb183b0717fce4717e1317
mahmoudalame <alamemany27@gmail.com>
2025-09-07 13:53:13 +0200

    SwiftUIButton

--- SwiftUIFuerYoutube/SwiftUIButton.swift
+++ SwiftUIFuerYoutube/SwiftUIButton.swift
@@ -9,10 +9,73 @@ import SwiftUI
 
 struct SwiftUIButton: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20){
+            Button("automatic title", action: {
+                print("buttoon pressed")
+            })
+            .font(.system(size: 50, weight: .bold))
+            .bold()
+            .buttonStyle(.automatic)
+            .tint(.red)
+            
+            
+            Button("bordered title", action: {
+                print("buttoon pressed")
+            })
+            .font(.system(size: 50, weight: .bold))
+            .bold()
+            .buttonStyle(.bordered)
+            .tint(.red)
+            
+            
+            Button("borderedProminent title", action: {
+                print("buttoon pressed")
+            })
+            .font(.system(size: 50, weight: .bold))
+            .bold()
+            .buttonStyle(.borderedProminent)
+            .tint(.red)
+            
+            Button("borderless title", action: {
+                print("buttoon pressed")
+            })
+            .font(.system(size: 50, weight: .bold))
+            .bold()
+            .buttonStyle(.borderless)
+            .tint(.red)
+            
+            
+            Button("plain title", action: {
+                print("buttoon pressed")
+            })
+            .font(.system(size: 50, weight: .bold))
+            .bold()
+            .buttonStyle(.plain)
+            .tint(.red)
+            
+            Button("plain title", action: {
+                print("buttoon pressed")
+            })
+            .font(.system(size: 50, weight: .bold))
+            .bold()
+            .buttonStyle(CustamButtonStyle())
+            
+        }
     }
 }
 
+
+struct CustamButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.title)
+            .bold()
+            .background(
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(.red))
+    }
+    
+}
 #Preview {
     SwiftUIButton()
 }
