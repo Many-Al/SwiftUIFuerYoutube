da9e731daae688d05701e0fa013e2a4f5ae5fadd
mahmoudalame <alamemany27@gmail.com>
2025-09-07 13:52:43 +0200

    SwiftUIButton

---
+++ SwiftUIFuerYoutube/SwiftUIButton.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIButton.swift
+//  SwiftUIFuerYoutube
+//
+//  Created by Mahmoud Alame on 07.09.25.
+//
+
+import SwiftUI
+
+struct SwiftUIButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIButton()
+}
