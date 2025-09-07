6856351045bbe09fb2f4ec60be221412164212ed
mahmoudalame <alamemany27@gmail.com>
2025-09-07 13:02:52 +0200

    SwiftUISafeArea

--- SwiftUIFuerYoutube/SwiftUISafeArea.swift
+++ SwiftUIFuerYoutube/SwiftUISafeArea.swift
@@ -9,22 +9,30 @@ import SwiftUI
 
 struct SwiftUISafeArea: View {
     var body: some View {
-      //  Color .red
+        //  Color .red
         //    .ignoresSafeArea(edges: [.top, .bottom])
-
-             VStack {
-            Text("Hello, World!")
-                     .font(.largeTitle)
-                     .bold()
-                 
-                 Spacer()
-                 
-                 Text("This is a preview")
-                     .font(.largeTitle)
-                     .bold()
-                 
+        
+        
+        ZStack {
+            Color.red
+              //  .ignoresSafeArea()
+            VStack {
+                Text("Hello, World!")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Spacer()
+                
+                Text("This is a preview")
+                    .font(.largeTitle)
+                    .bold()
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)// yemla el screen kilha
+            //.ignoresSafeArea(edges: [.top, .bottom])
+            //.background(ignoresSafeAreaEdges: .bottom, )
         }
-             .frame(maxWidth: .infinity, maxHeight: .infinity)// yemla el screen kilha
+        
     }
 }
 
