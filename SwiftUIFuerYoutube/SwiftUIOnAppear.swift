831d606e98cc2097c72e1ef87dc55def597bebe9
mahmoudalame <alamemany27@gmail.com>
2025-09-11 17:12:30 +0200

    SwiftUIOnAppear

--- SwiftUIFuerYoutube/SwiftUIOnAppear.swift
+++ SwiftUIFuerYoutube/SwiftUIOnAppear.swift
@@ -4,18 +4,32 @@
 //
 //  Created by Mahmoud Alame on 11.09.25.
 //
-
 import SwiftUI
 
 struct SwiftUIOnAppear: View {
-    @State var title = "hallo bibi"
+    @State var title = "hallo bibi" // Initialer Zustand
+@State var isAnimate: Bool = false
     var body: some View {
-        Text(title)
-            .font(.title)
-            .bold()
+        /*Text(title) // Anzeige des Titels
+            .font(.title) // Schriftgröße
+            .bold() // Schrift fett
             .onAppear(perform: {
-                title = "ich lerne swiftui"
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    title = "ich lerne swiftui" // Titel nach 3 Sekunden ändern
+                }
             })
+         */
+        Image(.image)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 300, height: 300, alignment: .center)
+            .foregroundStyle(.blue)
+            .offset(x: isAnimate ?  100 : -100, y: 0)
+            .animation(.easeInOut(duration: 2)  .repeatForever(), value: isAnimate)
+            .onAppear(perform: {
+                isAnimate = true
+            })
+        
         
         
         
